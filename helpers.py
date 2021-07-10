1b26f4531c607304e4d463074b2834509fdaab85
ahmad mustafa <ahmad.m.abdelqader@gmail.com>
2021-07-10 18:43:05 +0300

    Added watershed segmentation algorithm

--- helpers.py
+++ helpers.py
@@ -17,6 +17,8 @@ from scipy.ndimage import zoom
 import csv
 import numpy as np
 import SimpleITK as sitk
+import scipy.ndimage as ndimage
+from skimage import measure, morphology, segmentation
 def compute_dice(label_img, pred_img, p_threshold=0.5):
     p = pred_img.astype(numpy.float)
     l = label_img.astype(numpy.float)
@@ -460,3 +462,97 @@ def extractCube(scan, spacing, xyz, cube_size=80, cube_size_mm=51):
                     order=2)  # resample for cube_size
 
     return scancube
+
+#Ahmad July
+def generate_markers(image):
+    # Creation of the internal Marker
+    marker_internal = image < -400
+    marker_internal = segmentation.clear_border(marker_internal)
+    marker_internal_labels = measure.label(marker_internal)
+    areas = [r.area for r in measure.regionprops(marker_internal_labels)]
+    areas.sort()
+    if len(areas) > 2:
+        for region in measure.regionprops(marker_internal_labels):
+            if region.area < areas[-2]:
+                for coordinates in region.coords:
+                    marker_internal_labels[coordinates[0], coordinates[1]] = 0
+    marker_internal = marker_internal_labels > 0
+    # Creation of the external Marker
+    external_a = ndimage.binary_dilation(marker_internal, iterations=10)
+    external_b = ndimage.binary_dilation(marker_internal, iterations=55)
+    marker_external = external_b ^ external_a
+    # Creation of the Watershed Marker matrix
+    marker_watershed = np.zeros((512, 512), dtype=np.int)
+    marker_watershed += marker_internal * 255
+    marker_watershed += marker_external * 128
+
+    return marker_internal, marker_external, marker_watershed
+
+
+def seperate_lungs(image):
+    # Creation of the markers as shown above:
+    marker_internal, marker_external, marker_watershed = generate_markers(image)
+
+    # Creation of the Sobel-Gradient
+    sobel_filtered_dx = ndimage.sobel(image, 1)
+    sobel_filtered_dy = ndimage.sobel(image, 0)
+    sobel_gradient = np.hypot(sobel_filtered_dx, sobel_filtered_dy)
+    sobel_gradient *= 255.0 / np.max(sobel_gradient)
+
+    # Watershed algorithm
+    watershed = segmentation.watershed(sobel_gradient, marker_watershed)
+
+    # Reducing the image created by the Watershed algorithm to its outline
+    outline = ndimage.morphological_gradient(watershed, size=(3, 3))
+    outline = outline.astype(bool)
+
+    # Performing Black-Tophat Morphology for reinclusion
+    # Creation of the disk-kernel and increasing its size a bit
+    blackhat_struct = [[0, 0, 1, 1, 1, 0, 0],
+                       [0, 1, 1, 1, 1, 1, 0],
+                       [1, 1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1, 1],
+                       [0, 1, 1, 1, 1, 1, 0],
+                       [0, 0, 1, 1, 1, 0, 0]]
+    blackhat_struct = ndimage.iterate_structure(blackhat_struct, 8)
+    # Perform the Black-Hat
+    outline += ndimage.black_tophat(outline, structure=blackhat_struct)
+
+    # Use the internal marker and the Outline that was just created to generate the lungfilter
+    lungfilter = np.bitwise_or(marker_internal, outline)
+    # Close holes in the lungfilter
+    # fill_holes is not used here, since in some slices the heart would be reincluded by accident
+    lungfilter = ndimage.morphology.binary_closing(lungfilter, structure=np.ones((5, 5)), iterations=3)
+
+    # Apply the lungfilter (note the filtered areas being assigned -2000 HU)
+    segmented = np.where(lungfilter == 1, image, -2000 * np.ones((512, 512)))
+
+    return segmented, lungfilter, outline, watershed, sobel_gradient, marker_internal, marker_external, marker_watershed
+def segmentation_watershed(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    # Convert to int16 (from sometimes int16),
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+
+    image += np.int16(intercept)
+    images = np.array(image, dtype=np.int16) #test patient images
+    masks = []
+    for i in range(images.shape[0]):
+        lung_segmented, lung_lungfilter, lung_outline, lung_watershed, lung_sobel_gradient, \
+        lung_marker_internal, lung_marker_external, lung_marker_watershed = seperate_lungs(images[i])
+        #lung_patient_internal, lung_patient_external, lung_patient_watershed = generate_markers(images[i])
+        masks.append(lung_lungfilter)
+    return masks
